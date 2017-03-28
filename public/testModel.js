3b7a78d7f7a4d621c315c3f5a09712d8cff67aa1
afibrahim <a24ibrah@uwaterloo.ca>
2017-03-28 10:57:06 -0400

    Some modification in the code textModel.js
    modictication

--- public/testModel.js
+++ public/testModel.js
@@ -1,3 +1,5 @@
+// By Ahmed Ibrahim
+// Stock Mystery Project
 var ViewModel = function(services) {
     var self = this;
     self.services = ko.observableArray(services);
@@ -16,6 +18,3 @@ $.get( "/getServices", function(data) {
         });
     }, 3000);
 });
-
-
-
